75df7c30b3f9367d2f3ec4aca1f952414b8e10c4
Seonghun Jeong <tjdgns5506@gmail.com>
2025-05-25 20:27:42 +0900

    섹션 5-6 - 애노테이션 매핑 정보 사용

--- src/main/java/lecture/HelloController.java
+++ src/main/java/lecture/HelloController.java
@@ -1,7 +1,11 @@
 package lecture;
 
-import java.util.Objects;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Objects;
+@RequestMapping
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,6 +13,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
