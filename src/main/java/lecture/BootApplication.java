f1642aeb46aa2fce2b9ca771be6c9c60585a4291
Seonghun Jeong <tjdgns5506@gmail.com>
2025-05-25 16:18:01 +0900

    섹션 4-4
    - 코드로 서블릿 컨테이너 구성하여 실행

---
+++ src/main/java/lecture/BootApplication.java
@@ -0,0 +1,31 @@
+package lecture;
+
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BootApplication {
+    public static void main(String[] args) {
+        ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
+        servletWebServerFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    String name = req.getParameter("name");
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("hello " + name);
+                }
+            }).addMapping("/hello");
+        }).start();
+    }
+}
