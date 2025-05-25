904b190489d402fda924a9a623fcaec92a1a02a0
Seonghun Jeong <tjdgns5506@gmail.com>
2025-05-25 19:48:09 +0900

    섹션 5-4
    - 의존 오브젝트(SimpleHelloService) DI 적용

--- src/main/java/lecture/BootApplication.java
+++ src/main/java/lecture/BootApplication.java
@@ -4,7 +4,6 @@ package lecture;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,6 +18,7 @@ public class BootApplication {
     public static void main(String[] args) {
         GenericApplicationContext applicationContext = new GenericApplicationContext();
         applicationContext.registerBean(HelloController.class);
+        applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
 
         ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
---
+++ src/main/java/lecture/HelloService.java
@@ -0,0 +1,5 @@
+package lecture;
+
+public interface HelloService {
+    String sayHello(String name);
+}
