b8f9828c53f13c3e12e6627686d8dfb7482dc835
Seonghun Jeong <tjdgns5506@gmail.com>
2025-05-25 21:35:11 +0900

    31 빈의 생성주기 메서드(***) - 스프링 컨테이너의 마법 - 디스패처 서블릿의 ApplicationContextAware 이거 구현을 해서 스프링 컨테이너가 자동으로 디스패처 서블릿에 컨텍스트를 넣어주는것(?)

--- src/main/java/lecture/BootApplication.java
+++ src/main/java/lecture/BootApplication.java
@@ -3,6 +3,7 @@ package lecture;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,24 +13,33 @@ import org.springframework.web.servlet.DispatcherServlet;
 @ComponentScan
 public class BootApplication {
 
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
     public static void main(String[] args) {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
-                ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
+
+                ServletWebServerFactory servletWebServerFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                dispatcherServlet.setApplicationContext(this); // 이거 없어도 스프링이 알아서 이 작업을 함
+
                 servletWebServerFactory.getWebServer(servletContext -> {
-                            servletContext.addServlet("dispatcherServlet",
-                                    new DispatcherServlet(this)
-                            ).addMapping("/*");
+                            servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+                                    .addMapping("/*");
                         })
                         .start();
-
             }
         };
         applicationContext.register(BootApplication.class);
         applicationContext.refresh();//Template Method Pattern, 상속을 통해 기능 확장
-
-
     }
 }
