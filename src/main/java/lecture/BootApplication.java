64424c7a1e70ccf2f073387e0f47327a1b63254c
Seonghun Jeong <tjdgns5506@gmail.com>
2025-05-25 20:50:51 +0900

    29 자바코드 구성 정보 사용 - 팩토리 메서드 활용

--- src/main/java/lecture/BootApplication.java
+++ src/main/java/lecture/BootApplication.java
@@ -3,12 +3,26 @@ package lecture;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration // 스프링 구성 정보를 가진 클래스다 라는 표시
 public class BootApplication {
+    @Bean
+    public HelloController helloController(HelloService helloService) {// 의존 오브젝트를 파라미터로하면 스프링 컨테이너가 알아서 넘겨준다.
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
+
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -22,8 +36,7 @@ public class BootApplication {
 
             }
         };
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.register(BootApplication.class);
         applicationContext.refresh();//Template Method Pattern, 상속을 통해 기능 확장
 
 
