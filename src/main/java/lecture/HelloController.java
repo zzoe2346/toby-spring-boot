ad9f1bb3b755646d716362cbfb7d8febb5635e41
Seonghun Jeong <tjdgns5506@gmail.com>
2025-05-25 21:00:48 +0900

    30 `@Component` 스캔 - 팩토리 메서드 활용 - 메타 애너테이션 활용법 - RestController Controller ResponseBody 동작 과정

--- src/main/java/lecture/HelloController.java
+++ src/main/java/lecture/HelloController.java
@@ -1,11 +1,14 @@
 package lecture;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
-@RequestMapping
+
+@RestController
 public class HelloController {
     private final HelloService helloService;
 
@@ -14,7 +17,6 @@ public class HelloController {
     }
 
     @GetMapping("/hello")
-    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
---
+++ src/main/java/lecture/MyComponent.java
@@ -0,0 +1,14 @@
+package lecture;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Component //meta annotation
+public @interface MyComponent {
+}
